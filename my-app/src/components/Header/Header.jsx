14534c7de7b9c314d6de38d0cbcd5a763b8ca036
Atul Mishra <atulmishra@Atuls-MacBook-Air.local>
2025-02-26 21:12:03 +0530

    test1

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import search from "../../../public/icons/magnifying-glass.png"
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div className="container">
+          
+            <Image src={search} alt=''> </Image>
+            <img src={search} alt="search" />
+            <input type="text" placeholder=" Search Here..." className="search-bar" />
+          
+      </div>
+    </div>
   )
 }
