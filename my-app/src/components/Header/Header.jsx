e49681be0f46b9386bbb6a24f8715a77aed79383
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2025-02-26 21:34:02 +0530

    updated code

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import search from "../../../public/icons/magnifying-glass.png"
-import Image from 'next/image'
+import React from "react";
+import search from "../../../public/icons/magnifying-glass.png";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <div>
       <div className="container">
-          
-            <Image src={search} alt=''> </Image>
-            <img src={search} alt="search" />
-            <input type="text" placeholder=" Search Here..." className="search-bar" />
-          
+        <Image src={search} className="w-24 h-24" alt=""></Image>
+        <input
+          type="text"
+          placeholder=" Search Here..."
+          className="search-bar"
+        />
       </div>
     </div>
-  )
+  );
 }
