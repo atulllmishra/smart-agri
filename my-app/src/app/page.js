d2951c32a486174e9cf1bef496fc8f25123f7310
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2025-03-02 12:44:54 +0530

    Design Home Page

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,30 +1,272 @@
+"use client";
 import Image from "next/image";
 import search from "../../public/image1/agri.jpg";
+import React from "react";
 import productimage from "../../public/productimage/watermelon.webp";
 export default function Home() {
+
+
   return (
-    <div>
-      <div>
-      <Image src={search} alt="agri"  className="w-full h-screen opacity-15 bg-"/>
-     </div>
-      <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 pl-3">
-        <h1 className="text-6xl font-bold text-green-300">AnyaBazaar</h1>
-        <h2 className="text-2xl font-bold text-green-320">Agriculture is our wisest pursuit, because it will in the end contribute most to real wealth, good morals, and happiness.</h2>
+    <div className="w-full h-screen relative">
+      <div className="w-full">
+        <Image
+          src={search}
+          alt="agri"
+          className="w-full h-screen opacity-100"
+        />
       </div>
-      <div>
-        <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 px-10 py-6 mx-12 bg-black opacity-85 rounded-sm">
+        <div className="">
+          <p className="text-2xl font-bold text-green-300">
+            Welcome to Annyabazar
+          </p>
+          <p className=" font-bold text-white mb-4">
+            Your Ultimate Agricultural Products Trading Platform !!
+          </p>
+          <p className="text-white ">
+            At FasalMandi, we are committed to changing the way farmers,
+            suppliers and buyers connect and do business in agriculture. Our
+            platform allows small business owners to buy fresh produce directly
+            from farmers and farmers to sell their produce directly to
+            retailers, eliminating the middleman and keeping prices fair.
+          </p>
+          <p className="text-white my-3">
+            Join us in building the future of Indian agriculture. Explore our
+            online store where you can buy and sell products, buy wholesale and
+            be part of a large network of retailers-farmers.
+          </p>
 
-          title="Watermelon"
-          price="@56"
-          image="/productimage/watermelon.webp"
-          description="Newly launched!"
-
+          <button className="bg-green-500 text-white px-4 py-2 my-5 rounded-md">
+            Join Now
+          </button>
         </div>
       </div>
-      <div>
-      </div>
+
+      <section className="w-full bg-white flex flex-col">
+        <h1 className="text-3xl font-bold text-center py-5">Our Products</h1>
+        <div className="grid grid-rows-3 place-items-center gap-8">
+          <div className="grid grid-cols-4 gap-5">
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-5">
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col border-2 border-slate-400  border-solid p-2">
+              <div className="flex flex-col items-center border-2 w-56">
+                <Image src={productimage} className="w-40 h-40" alt="product" />
+              </div>
+              <div className="w-full flex flex-row justify-between my-2">
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Product Name</p>
+                  <p className="font-bold text-gray-500">Quantity Availabe</p>
+                  <p className="font-bold text-gray-500">Unit price</p>
+                  <p className="font-bold text-gray-500">Upload Time</p>
+                </div>
+                <div className="flex flex-col text-xs gap-1">
+                  <p className="font-bold">Watermelon</p>
+                  <p className="font-bold text-gray-500">50 crt</p>
+                  <p className="font-bold text-gray-500">250 INR</p>
+                  <p className="font-bold text-gray-500">2025-03-02</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-yellow-300 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Buy Now
+                </button>
+                <div className="bg-green-600 text-black text-xs px-3 py-1  my-1 rounded-md">
+                  Rated: 4/5
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
   );
 }
--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,49 +1,75 @@
+"use client";
+
 import React from "react";
 import search from "../../../public/icons/magnifying-glass.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Signup from "@/app/(Authentication)/signup/page.jsx"
 import Link from "next/link";
+
 export default function Header() {
+
+  const router = useRouter();
+
+  const handleSelectChange = (e) => {
+    const value = e.target.value;
+    if(value) {
+      router.push(value);
+    }
+  }
+  
   return (
-    <div className="bg-white text-wrap">
+    <div className="w-full flex flex-col bg-white">
       <div className="flex justify-evenly items-center w-full">
-        <div className="container flex items-center pt-2px pb-2px h-16 pl-4">
-          <Image src={search} className="w-9 h-9 flex items-center bg-white" alt=""></Image>
-          <input className="flex justify-center h-auto w-96 p-2 border border-green-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 "
-            type="text"
-            placeholder=" Search Here..."
-          />
-          <div className="items-right pl-16">
-            <select name="" id="" className="text-black font-bold">
-              <option value="">Login/SignUp as Seller</option>
-              <option value="">Login/SignUp as Buyer</option>
+        <div className="container flex items-center justify-around p-2">
+          <div>Logo</div>
+          <div className="w-full flex justify-center items-center">
+            <input
+              className="flex justify-center h-auto w-96 p-2 border border-green-600 rounded-sm shadow-sm focus:ring-blue-500 focus:border-blue-500 "
+              type="text"
+              placeholder=" Search Here..."
+            />
+            <Image
+              src={search}
+              className="w-10 h-10 flex items-center bg-green-600 rounded-sm p-1"
+              alt=""
+            ></Image>
+          </div>
+          <div className="">
+            <select name="" onChange={handleSelectChange} id="" className="text-black font-bold">
+              <option value="/login">Login</option>
+              <option value="/signup">Signup</option>
             </select>
           </div>
         </div>
       </div>
-      <div className="bg-green-300 h-12 w-full text-wrap">
-        <div className="container flex items-center justify-between pl-4 pr-4 pt-2">
-          <div className="flex items-center">
-            <div className="flex items-center">
-              <h1 className="text-white font-bold">Smart Agri</h1>
-            </div>
-          </div>
-          <div className="flex items-center justify-around text-black ">
-            <div className="flex items-center pl-4 pr-4 ">
-              <h1 className=" font-bold"><a href="home.html">Home</a></h1>
-            </div>
-            <div className="flex items-center pl-4 pr-4">
-              <h1 className="font-bold"><Link href="/aboutus">About Us</Link></h1>
-            </div>
-            <div className="flex items-center pl-4 pr-4">
-              <h1 className="font-bold"><Link href="/contactus">Contact Us</Link></h1>
-            </div>
-            <div className="flex items-center pl-4 pr-4">
-              <h1 className="font-bold"><a href="services.html">Services</a></h1>
-            </div>
-            <div className="flex items-center pl-4 pr-4">
-              <h1 className="font-bold"><a href="faq.html">FAQ</a></h1>
-            </div>
-          </div>
+
+      <div className="w-full bg-green-600 place-items-center justify-center m-auto">
+        <div className="w-1/2 flex items-center justify-around text-white py-3">
+          <h1 className="font-poppins">
+            <Link href="/">Home</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">Sell</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">Buy</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">Services</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">About Us</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">Terms & Services</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">Contact</Link>
+          </h1>
+          <h1 className="font-poppins">
+            <Link href="/">FAQ</Link>
+          </h1>
         </div>
       </div>
     </div>
