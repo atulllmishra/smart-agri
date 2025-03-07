1d7a98def2ecc3ec0f3655c3beece26af786a992
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2025-03-07 09:03:34 +0530

    fix deployed issues

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["drive.google.com", "images.pexels.com"], // Corrected array
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- my-app/src/app/api/Processor/route.js
+++
@@ -1,8 +0,0 @@
-class AudioProcessor extends AudioWorkletProcessor {
-    process(inputs, outputs, parameters) {
-        return true; // Pass-through processor (not modifying audio)
-    }
-}
-
-// Properly register the processor
-registerProcessor("audio-processor", AudioProcessor);
